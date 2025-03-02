7a2e4646c02e797d7cbf73ddc2c08626ce2b85cc
leticia-chijo <leticia.chijo@gmail.com>
2025-03-02 16:13:54 -0300

    Aula 09

--- navegacao/app/_layout.jsx
+++ navegacao/app/_layout.jsx
@@ -1,5 +1,21 @@
-import { Text } from "react-native"
+import { Stack } from "expo-router"
+import { StatusBar } from "expo-status-bar"
 
 export default function RootLayout() {
-  return <Text>Navegação</Text>
+  return (
+    <>
+      <StatusBar style="light" backgroundColor="#E94560" />
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: "#E94560" },
+          headerTintColor: "#FFFFFF"
+          // headerShown: false
+        }}
+      >
+        <Stack.Screen name="index" options={{ headerTitle: "Home" }} />
+        <Stack.Screen name="settings" options={{ headerTitle: "Configurações" }} />
+        <Stack.Screen name="user" options={{ headerTitle: "Usuário" }} />
+      </Stack>
+    </>
+  )
 }
