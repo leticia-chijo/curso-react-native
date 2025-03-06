d990fba856f03d7e954806510512cc1f366185f5
leticia-chijo <leticia.chijo@gmail.com>
2025-03-06 17:24:00 -0300

    Aula 22

--- money/app/(tabs)/add-transactions.jsx
+++ money/app/(tabs)/add-transactions.jsx
@@ -1,133 +1,42 @@
-import {
-  Text,
-  View,
-  ScrollView,
-  TextInput,
-  Alert,
-  StyleSheet,
-  TouchableOpacity,
-  Platform
-} from "react-native"
+import { View, ScrollView, Alert, StyleSheet, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback } from "react-native"
 import { globalStyles } from "../../styles/globalStyles"
 import Button from "../../components/Button"
-import { useState } from "react"
-import RNDateTimePicker from "@react-native-community/datetimepicker"
-import { Picker } from "@react-native-picker/picker"
-import { categories } from "../../constants/categories"
-import { colors } from "../../constants/colors"
+import { useRef, useState } from "react"
+import CategoryPicker from "../../components/CategoryPicker"
+import DatePicker from "../../components/DatePicker"
+import CurrencyInput from "../../components/CurrencyInput"
+import DescriptionInput from "../../components/DescriptionInput"
+
+const initialForm = {
+  description: "",
+  value: 0,
+  date: new Date(),
+  category: "Renda"
+}
 
 export default function AddTransactions() {
-  const initialForm = {
-    description: "",
-    value: 0,
-    date: new Date(),
-    category: "Renda"
-  }
-
   const [form, setForm] = useState(initialForm)
-  const [showPicker, setShowPicker] = useState(false)
+  const valueInputRef = useRef()
 
   const addTransaction = () => {
     Alert.alert(`${form.description} | ${form.value} | ${form.date} | ${form.category}`)
   }
 
-  const handleCurrencyChange = (text) => {
-    const formattedValue = text.replace(/\D/g, "")
-    const numberValue = formattedValue ? parseFloat(formattedValue) / 100 : 0
-
-    setForm({ ...form, value: numberValue })
-  }
-
-  const handleDateChange = (_, selectDate) => {
-    setShowPicker(false)
-
-    if (selectDate) {
-      setForm({ ...form, date: selectDate })
-    }
-  }
-
   return (
-    <View style={globalStyles.screenContainer}>
-      <ScrollView style={globalStyles.content}>
-        <View style={styles.form}>
-          <View>
-            <Text style={globalStyles.inputLabel}>Descrição</Text>
-            <TextInput
-              value={form.description}
-              onChangeText={(text) => setForm({ ...form, description: text })}
-              style={globalStyles.input}
-            />
+    <KeyboardAvoidingView style={globalStyles.screenContainer}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView style={globalStyles.content}>
+          <View style={styles.form}>
+            <DescriptionInput form={form} setForm={setForm} valueInputRef={valueInputRef} />
+            <CurrencyInput form={form} setForm={setForm} valueInputRef={valueInputRef}/>
+            <DatePicker form={form} setForm={setForm} />
+            <CategoryPicker form={form} setForm={setForm} />
           </View>
 
-          <View>
-            <Text style={globalStyles.inputLabel}>Valor</Text>
-            <TextInput
-              value={form.value.toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL"
-              })}
-              onChangeText={handleCurrencyChange}
-              keyboardType="numeric"
-              style={globalStyles.input}
-            />
-          </View>
-
-          <View>
-            <Text style={globalStyles.inputLabel}>Data</Text>
-            <TouchableOpacity onPress={() => setShowPicker(true)}>
-              <TextInput
-                value={form.date.toLocaleDateString("pt-BR")}
-                onChangeText={(text) => setForm({ ...form, date: text })}
-                style={globalStyles.input}
-                editable={false}
-              />
-            </TouchableOpacity>
-
-            {showPicker && (
-              <RNDateTimePicker
-                mode="date"
-                display={Platform.OS === "ios" ? "inline" : "default"}
-                value={form.date}
-                onChange={handleDateChange}
-              />
-            )}
-          </View>
-
-          <View>
-            <Text style={globalStyles.inputLabel}>Categoria</Text>
-            <View style={styles.picker}>
-              <Picker
-                selectedValue={form.category}
-                onValueChange={(itemValue) => setForm({ ...form, category: itemValue })}
-              >
-                <Picker.Item
-                  label={categories.income.displayName}
-                  value={categories.income.name}
-                />
-                <Picker.Item
-                  label={categories.food.displayName}
-                  value={categories.food.name}
-                />
-                <Picker.Item
-                  label={categories.house.displayName}
-                  value={categories.house.name}
-                />
-                <Picker.Item
-                  label={categories.education.displayName}
-                  value={categories.education.name}
-                />
-                <Picker.Item
-                  label={categories.travel.displayName}
-                  value={categories.travel.name}
-                />
-              </Picker>
-            </View>
-          </View>
-        </View>
-
-        <Button onPress={addTransaction}>Adicionar</Button>
-      </ScrollView>
-    </View>
+          <Button onPress={addTransaction}>Adicionar</Button>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   )
 }
 
@@ -136,14 +45,5 @@ const styles = StyleSheet.create({
     gap: 12,
     marginBottom: 40,
     marginTop: 10
-  },
-  picker: {
-    display: "flex",
-    justifyContent: "center",
-    height: 44,
-    borderColor: colors.secondaryText,
-    borderWidth: 1,
-    borderRadius: 8,
-    flexGrow: 1
   }
 })
